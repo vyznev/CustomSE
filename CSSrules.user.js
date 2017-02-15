7174644203250f8039328dbeef5997da30fea5c1
Normal Human <normalhuman@users.noreply.github.com>
2017-02-14 23:35:37 -0500

    simplify rules, don't exclude chat and area51

--- CSSrules.user.js
+++ CSSrules.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Custom SE styles
 // @namespace   https://github.com/normalhuman/CustomSE
-// @description Several rules that hide annoying features 
+// @description Several rules that hide annoying features
 // @match       *://*.askubuntu.com/*
 // @match       *://*.mathoverflow.net/*
 // @match       *://*.serverfault.com/*
@@ -9,15 +9,12 @@
 // @match       *://*.stackexchange.com/*
 // @match       *://*.stackoverflow.com/*
 // @match       *://*.superuser.com/*
-// @exclude     *://area51.*
-// @exclude     *://discuss.area51.*
-// @exclude     *://chat.*
 // @grant       GM_addStyle
 // @run-at      document-start
-// @version     17.2.1
+// @version     17.2.2
 // ==/UserScript==
 
-GM_addStyle('.topbar, #header, .so-header, #hireme, #explore-tags, #hot-network-questions, #newsletter-ad, #ignored-footer, .bottom-notice, .adzerk-vote  {display: none;} .icon-achievements, .icon-flag, .cite-link {  display: none !important; }  .downvoted-answer { opacity: 1 !important; } div.tagged-interesting {  background-color: #fff !important; }');
+GM_addStyle('.topbar, #header, .so-header, #hireme, #explore-tags, #hot-network-questions, #newsletter-ad, #ignored-footer, .bottom-notice, .adzerk-vote  {display: none;}  .cite-link {  display: none !important; }  div.tagged-interesting {  background-color: #fff !important; }');
 
 if (/^math\./.test(window.location.hostname)) {
   GM_addStyle('.result-link a:visited {color: #507070 !important;}');
