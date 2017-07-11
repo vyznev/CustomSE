81d75e50e05518b8197b658f9b4004708370a82d
Normal Human <normalhuman@users.noreply.github.com>
2017-07-11 17:01:43 -0400

    don't include child metas when searching metaSE

--- GoogleSearch.user.js
+++ GoogleSearch.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        Search SE sites with Google 
+// @name        Search SE sites with Google
 // @namespace   https://github.com/normalhuman/CustomSE
 // @description Pressing Alt-Enter in SE search box invokes Google search in a new tab.
 // @match       *://*.askubuntu.com/*
@@ -12,30 +12,32 @@
 // @exclude     *://chat.*
 // @grant       GM_openInTab
 // @run-at      document-end
-// @version     15.8.1
+// @version     17.7.1
 // ==/UserScript==
 
 
-var smallBox = document.querySelector('#search input');
+var smallBox = document.querySelector("#search input");
 if (smallBox) {
   smallBox.addEventListener("keydown", function(e) {
-    if (e.keyCode == 13 && e.altKey) {    
+    if (e.keyCode == 13 && e.altKey) {
       google(smallBox.value);
     }
   });
 }
 
-var largeBox = document.querySelector('#bigsearch input');
+var largeBox = document.querySelector("#bigsearch input");
 if (largeBox) {
   largeBox.addEventListener("keydown", function(e) {
-    if (e.keyCode == 13 && e.altKey) {    
+    if (e.keyCode == 13 && e.altKey) {
       google(largeBox.value);
-    }  
+    }
   });
 }
 
 
 function google(string) {
-  var url = encodeURI('https://google.com/search?q='+string+' site:'+window.location.hostname).replace(/\+/g,'%2B');  
+  var host = window.location.hostname;
+  var param = " site:" + host + (host == "meta.stackexchange.com" ? " -site:*.meta.stackexchange.com" : "");
+  var url = encodeURI("https://google.com/search?q=" + string + param).replace(/\+/g,"%2B");
   GM_openInTab(url);
 }
